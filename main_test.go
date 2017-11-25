ad3c0638a28649348fa0673d419d703b8951f9f8
fcu-d0342315 <blowball85830@gmail.com>
2017-11-25 15:06:03 +0800

    New feature.

--- main_test.go
+++ main_test.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-        "testing"
+       "testing"
 )
 
 func TestAdd(t *testing.T) {
-        result := Add(1, 3)
-        if result != 4 {
-                t.Errorf("should be 4 but %d", result)
-        }
+       result := Add(1, 3)
+       if result != 4 {
+               t.Errorf("should be 4 but %d", result)
+       }
 }
+
+func TestDiv(t *testing.T) {
+       result,err := Div(4, 2)
+       if err!=nil{
+               t.Error(err.Error())
+       }
+       if result != 2 {
+               t.Errorf("should be 4 but %d", result)
+       }
